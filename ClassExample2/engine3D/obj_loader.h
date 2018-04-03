99f939e72b9f317a0e50f45d81707c4f39dabcfd
TalBarami <talbaramii@gmail.com>
2018-04-03 12:11:05 +0300

    Added edges & initialization

---
+++ ClassExample2/engine3D/obj_loader.h
@@ -0,0 +1,68 @@
+#ifndef OBJ_LOADER_H_INCLUDED
+#define OBJ_LOADER_H_INCLUDED
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <list>
+#include <string>
+#include <map>
+
+struct OBJIndex
+{
+    unsigned int vertexIndex;
+    unsigned int uvIndex;
+    unsigned int normalIndex;
+	unsigned int edgeIndex;
+	std::list<OBJIndex>::iterator faceIndex;
+	unsigned int tmpIndx;
+    bool isEdgeUpdated;
+    bool operator<(const OBJIndex& r) const { return vertexIndex < r.vertexIndex; }
+	bool operator==(const OBJIndex& r) const { return vertexIndex == r.vertexIndex; }
+};
+
+struct Edge
+{
+	OBJIndex* vertex1;
+	OBJIndex* vertex2;
+	double error;
+};
+
+struct IndexedModel
+{
+
+    std::vector<glm::vec3> positions;
+    std::vector<glm::vec2> texCoords;
+    std::vector<glm::vec3> normals;
+	std::vector<glm::vec3> colors;
+    std::vector<unsigned int> indices;
+};
+
+class OBJModel
+{
+public:
+    std::list<OBJIndex> OBJIndices;
+    std::vector<glm::vec3> vertices;
+	std::vector<Edge> edges;
+    std::vector<glm::vec2> uvs;
+    std::vector<glm::vec3> normals;
+	std::vector<glm::vec3> colors;
+
+	bool hasUVs;
+    bool hasNormals;
+    
+    OBJModel(const std::string& fileName);
+    
+    IndexedModel ToIndexedModel();
+private:
+    unsigned int FindLastVertexIndex(const std::vector<OBJIndex*>& indexLookup, const OBJIndex* currentIndex, const IndexedModel& result);
+    void CreateOBJFace(const std::string& line);
+    
+    glm::vec2 ParseOBJVec2(const std::string& line);
+    glm::vec3 ParseOBJVec3(const std::string& line);
+    OBJIndex ParseOBJIndex(const std::string& token, bool* hasUVs, bool* hasNormals);
+	Edge CreateEdge(OBJIndex v1, OBJIndex v2);
+	void CalcNormals();
+
+};
+
+#endif // OBJ_LOADER_H_INCLUDED
